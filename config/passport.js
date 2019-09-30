df3baaefb1f941af64ad12db37906255400243cd
Aman1kr <amankumar91299@gmail.com>
2019-09-30 08:37:23 +0530

    validation1

--- config/passport.js
+++ config/passport.js
@@ -11,6 +11,7 @@ opts.secretOrKey = keys.secretOrKey;
 
 module.exports = passport => {
     passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+      
         User.findById(jwt_payload.id)
         .then(user => {
             if(user){
--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,11 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+
+//Load input validation
+
+const validateRegisterInput = require('../../validation/register');
+
 // Load User model
 const User = require('../../models/User');
 
@@ -18,6 +23,15 @@ router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
 // @desc    Register user
 // @access  Public
 router.post('/register', (req, res) => {
+
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    //Check Validation
+    if(!isValid){
+      return res.status(400).json(errors);
+    }
+
+
     User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       errors.email = 'Email already exists';
